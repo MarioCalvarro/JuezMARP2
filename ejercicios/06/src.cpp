747a0e7c1cdcbeb12195b1279b19d288815dd50d
Mario <mcalvarr@ucm.es>
2024-03-04 17:19:16 +0100

    06.1

--- ejercicios/06/src.cpp
+++ ejercicios/06/src.cpp
@@ -15,39 +15,40 @@ using namespace std;
 using cofre_t = pair<size_t, size_t>;
 
 //first: oro total
-//second: cofres elegidos
-using res_t = pair<size_t, vector<bool>>;
+//second: cofres elegidos y número
+using res_t = pair<size_t, pair<vector<bool>, size_t>>;
 
 //a_{i,j} = valor con j seg restante y considerados los objetos hasta i
 using Matrix = vector<vector<size_t>>;
 
 res_t resolver(size_t segundos_disp, const vector<cofre_t>& cofres) {
-    size_t n = cofres.size(), m = segundos_disp+1;
+    size_t n = cofres.size() + 1, m = segundos_disp+1;
     Matrix tabla = Matrix(n, vector<size_t>(m, 0));
 
     for (size_t i = 1; i < n; ++i) {
+        size_t coste_real = 3*cofres[i-1].first;
         for (size_t ii = 1; ii < m; ++ii) {
-            size_t coste_real = 3*cofres[i].first;
             if (coste_real > ii) {
                 tabla[i][ii] = tabla[i-1][ii];
             }
             else {
                 tabla[i][ii] = max(tabla[i-1][ii],
-                                   tabla[i-1][ii - coste_real] + cofres[i].second);
+                                   tabla[i-1][ii - coste_real] + cofres[i-1].second);
             }
         }
     }
 
-    size_t resto = segundos_disp;
-    vector<bool> elegidos = vector<bool>(n, false);
-    for (size_t i = n; i > 1; --i) {
-        if (!(tabla[i-1][resto] == tabla[i-2][resto])) {
-            elegidos[i] = true;
-            resto -= cofres[i].second;
+    size_t resto = segundos_disp, total = 0;
+    vector<bool> elegidos = vector<bool>(cofres.size(), false);
+    for (size_t i = n-1; i > 0; --i) {
+        if (!(tabla[i][resto] == tabla[i-1][resto])) {
+            elegidos[i-1] = true;
+            resto -= 3*cofres[i-1].first;
+            total += 1;
         }
     }
 
-    return {tabla[n-1][segundos_disp], elegidos};
+    return {tabla[n-1][segundos_disp], {elegidos, total}};
 }
 
 bool resuelveCaso() {
@@ -67,9 +68,9 @@ bool resuelveCaso() {
 
     res_t sol = resolver(segundos_disp, cofres);
 
-    cout << sol.first << '\n' << sol.second.size() << '\n';
-    for (size_t i = 0; i < sol.second.size(); ++i) {
-        if (sol.second[i]) {
+    cout << sol.first << '\n' << sol.second.second << '\n';
+    for (size_t i = 0; i < sol.second.first.size(); ++i) {
+        if (sol.second.first[i]) {
             cout << cofres[i].first << ' ' << cofres[i].second << '\n';
         }
     }
