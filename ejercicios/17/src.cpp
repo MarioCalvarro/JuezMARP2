8119f05e92aaf21fccd930defac0fae1ad0e05d3
mario <mcalvarr@ucm.es>
2024-05-22 20:12:57 +0200

    17

--- ejercicios/17/src.cpp
+++ ejercicios/17/src.cpp
@@ -2,53 +2,131 @@
 
 #include <iostream>
 #include <fstream>
+#include <queue>
 #include <utility>
 #include <vector>
-#include <limits.h>
+#include <algorithm>
 
 using namespace std;
 
+using matriz_t = vector<vector<size_t>>;
+using cota_t = vector<size_t>;
 using res_t = size_t;
-//Min, formas
-using dato_t = pair<size_t, size_t>;
 
-res_t resolver(size_t pagar, const vector<size_t> &monedas)
+struct Nodo {
+    size_t k, tiempo, optimista;
+    vector<bool> asignado;
+};
+
+bool operator<(const Nodo& lhs, const Nodo& rhs) {
+    return lhs.optimista > rhs.optimista;
+}
+
+void rellenar_cota_inferior(cota_t &cota, const matriz_t &m) {
+    size_t n = cota.size();
+    size_t min_fila = 10001;
+
+    for (size_t j = 0; j < n; ++j) {
+        min_fila = min(min_fila, m[n-1][j]);
+    }
+    cota[n - 1] = min_fila;
+
+    for (size_t i = n-1; i > 0; i--) {
+        size_t aux_i = i - 1;
+        min_fila = 10001;
+
+        for (size_t j = 0; j < n; ++j) {
+            min_fila = min(min_fila, m[aux_i][j]);
+        }
+
+        cota[aux_i] = min_fila + cota[aux_i + 1];
+    }
+}
+
+void rellenar_cota_superior(cota_t &cota, const matriz_t &m) {
+    size_t n = cota.size();
+    size_t max_fila = 0;
+
+    for (size_t j = 0; j < n; ++j) {
+        max_fila = max(max_fila, m[n-1][j]);
+    }
+    cota[n - 1] = max_fila;
+
+    for (size_t i = n-1; i > 0; i--) {
+        size_t aux_i = i - 1;
+        max_fila = 0;
+
+        for (size_t j = 0; j < n; ++j) {
+            max_fila = max(max_fila, m[aux_i][j]);
+        }
+
+        cota[aux_i] = max_fila + cota[aux_i + 1];
+    }
+}
+
+res_t resolver(const matriz_t &m, const cota_t &c_inf, const cota_t &c_sup)
 {
-    size_t n = monedas.size();
-    vector<dato_t> v(pagar + 1, {INT_MAX, 0});
-    v[0] = {0, 1};
+    size_t n = m.size();
 
-    for (size_t i = 0; i < n; ++i) {
-        for (size_t j = monedas[i]; j < pagar + 1; ++j) {
-            //Nuevo mínimo
-            if (v[j - monedas[i]].first + 1 < v[j].first) {
-                v[j] = {v[j - monedas[i]].first + 1, v[j - monedas[i]].second};
-            }
-            //Otra forma para el mismo mínimo
-            else if (v[j - monedas[i]].first + 1 == v[j].first) {
-                v[j].second += v[j - monedas[i]].second;
+    //Raíz
+    Nodo y = {0, 0, 0, vector<bool>(n, false)};
+    size_t mejor_tiempo = y.tiempo + c_sup[y.k];      //Cota superior
+
+    priority_queue<Nodo> cola;
+    cola.push(y);
+
+    //Cota inferior
+    while (!cola.empty() && (cola.top().tiempo + c_inf[cola.top().k]) <= mejor_tiempo) {
+        y = cola.top(); cola.pop();
+
+        for (size_t i = 0; i < n; ++i) {
+            if (!y.asignado[i]) {
+                Nodo x = {y.k + 1, y.tiempo + m[y.k][i], 0, y.asignado};
+                x.optimista = x.tiempo + c_inf[x.k];    //Cota inferior
+                x.asignado[i] = true;
+
+                if (x.k == n) {     //Solución?
+                    if (x.tiempo < mejor_tiempo) {
+                        mejor_tiempo = x.tiempo;
+                    }
+                } else {
+                    if (x.optimista <= mejor_tiempo) {
+                        cola.push(x);
+
+                        //Cota superior
+                        if ((x.tiempo + c_sup[x.k]) < mejor_tiempo) {
+                            mejor_tiempo = x.tiempo + c_sup[x.k];
+                        }
+                    }
+                }
             }
         }
-    }    
+    }
 
-    return v[pagar].second;
+    return mejor_tiempo;
 }
 
 bool resuelveCaso()
 {
     //Leer
-    size_t pagar, n;
-    cin >> pagar >> n;
+    size_t n;
+    cin >> n;
 
-    if (!std::cin)
+    if (n == 0)
         return false;
 
-    vector<size_t> monedas(n);
-    for (auto &m : monedas) {
-        cin >> m;
+    matriz_t matriz (n, vector<size_t>(n, 0));
+    for (auto &row : matriz) {
+        for (auto &col : row) {
+            cin >> col;
+        }
     }
 
-    res_t sol = resolver(pagar, monedas);
+    cota_t cota_inferior(n), cota_superior(n);
+    rellenar_cota_inferior(cota_inferior, matriz);
+    rellenar_cota_superior(cota_superior, matriz);
+
+    res_t sol = resolver(matriz, cota_inferior, cota_superior);
 
     //Escribir
     cout << sol << '\n';
