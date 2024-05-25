a83f4f422c6d165135eb1248c1d36e710f3e9360
mario <mcalvarr@ucm.es>
2024-05-25 20:10:23 +0200

    29

---
+++ ejercicios/29/src.cpp
@@ -0,0 +1,69 @@
+// DG, Mario Calvarro Marines
+
+#include <iostream>
+#include <fstream>
+#include <vector>
+
+using namespace std;
+
+using res_t = int;
+using matriz_t = vector<vector<int>>;
+
+res_t resolver(const string &s)
+{
+    size_t n = s.size();
+    vector<vector<int>> m(n, vector<int>(n, n));
+
+    for (size_t i = 0; i < n; i++) {
+        m[i][i] = 1; 
+    }
+    
+    for (int i = n - 2; 0 <= i; i--) {
+        for (size_t j = i + 1; j < n; j++) {
+            if (s[i] == s[i + 1]) {
+                m[i][j] = min(m[i][j], m[i + 1][j]);
+            }
+            if (s[j] == s[j - 1]) {
+                m[i][j] = min(m[i][j], m[i][j - 1]);
+            }
+            if (s[i] == s[j]) {
+                m[i][j] = min(m[i][j], m[i + 1][j - 1] + 1);
+            }
+
+            m[i][j] = min(m[i][j], std::min(m[i + 1][j], m[i][j - 1]) + 1);
+        }
+    }
+    return m[0][n - 1];
+}
+
+bool resuelveCaso() 
+{
+    //Leer
+    string s;
+    cin >> s;
+
+    if (!std::cin)
+        return false;
+
+    res_t sol = resolver(s);
+
+    //Escribir
+    cout << sol << '\n';
+
+    return true;
+}
+
+int main() {
+#ifndef DOMJUDGE
+    std::ifstream in("casos.txt");
+    auto cinbuf = std::cin.rdbuf(in.rdbuf());
+#endif
+
+    while(resuelveCaso());
+
+#ifndef DOMJUDGE
+    std::cin.rdbuf(cinbuf);
+#endif
+
+    return 0;
+}
